e706188f57a5bc82585adc2a6ce3c7717d0e0fcd
Dr.Mahmoud <82735653+dentist118@users.noreply.github.com>
2022-12-27 23:24:21 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.models import model_from_json
@@ -17,11 +16,11 @@ loaded_model.load_weights("./model.h5")
 
 with open('./bart-chalkboard-data.txt','r',encoding='utf-8')as file:
     data=file.read()
-def generate_text(model,Tokenizer,max_length,seed_text,n_words):
+def generate_text(model,tokenizer,max_length,seed_text, n_words):
     text_generated =seed_text
-    for i in range(int(n_words)):
+    for i in range(int( n_words) ):
         encoded=tokenizer.texts_to_sequences([text_generated])[0]
-        encoded=pad_sequences([encoded],max_len=max_length,padding='pre')
+        encoded=pad_sequences([encoded],maxlen=max_length,padding='pre')
         yhat=model.predict_classes(encoded,verbose=0)
         predicted_word=''
         for word,index in tokenizer.word_index.items():
@@ -35,12 +34,12 @@ tokenizer.fit_on_texts([data])
 max_length =14
 st.title("The Simpsons Chalkboards Gag Text Generator.")
 image =Image.open('./1.jpg')
-st.Image(image,use_column_width=True)
+st.image(image,use_column_width=True)
 n_words= st.number_input('Type the number of  words you to gemerate ')
-seed_text= st.Text_input('Type the number of words you want to generate after')
+seed_text= st.text_input('Type a word or words you want to generate after')
 
 
 if n_words and seed_text:
-    st.header(generate_text(loaded_model,tokenizer,max_length-1,seed_text,n_words))
+    st.header(generate_text(loaded_model , tokenizer , max_length-1 , seed_text, n_words))
 else:
     st.warning("Please input a word and a number")
